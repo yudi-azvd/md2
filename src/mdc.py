7b8cbbe901d56a285e040b79f1cabb8a51fdb435
Yudi Yamane <yudiyamane@gmail.com>
2022-06-28 22:17:27 -0300

    feat: adiciona duas implementações de MDC

--- src/mdc.py
+++ src/mdc.py
@@ -1,2 +1,12 @@
-def mdc(a: int, b: int) -> int:
-  return 0
+def mdc_r(a: int, b: int) -> int:
+  if b == 0:
+    return a
+  return mdc_r(b, a % b)
+
+def mdc_i(a: int, b: int) -> int:
+  while b != 0:
+    new_b = a % b
+    a = b
+    b = new_b
+  return a
+  
--- test/test_mdc.py
+++ test/test_mdc.py
@@ -1,15 +1,22 @@
 import pytest
 
-from mdc import mdc
+from mdc import mdc_i, mdc_r
 
 test_data = [
-  (15, 20, 5)
+  (15, 20, 5),
+  (19, 9, 1),
+  (18, 9, 9),
+  (48, 18, 6),
 ]
 
 params = pytest.mark.parametrize("a, b, expected_mdc", test_data)
 
+@params
+def test_mdc_r(a: int, b: int, expected_mdc: int):
+  result_mdc = mdc_r(a, b)
+  assert expected_mdc == result_mdc
 
 @params
-def test_mdc_1(a: int, b: int, expected_mdc: int):
-  result_mdc = mdc(a, b)
+def test_mdc_i(a: int, b: int, expected_mdc: int):
+  result_mdc = mdc_i(a, b)
   assert expected_mdc == result_mdc
