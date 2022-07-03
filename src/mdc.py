3563afd5f0582c4870b5db91f7a82f7474c92f6e
Yudi Yamane <yudiyamane@gmail.com>
2022-07-03 01:20:48 -0300

    docs: atualiza comentário para funções de MDC

--- src/mdc.py
+++ src/mdc.py
@@ -1,13 +1,16 @@
 from typing import Set
 
 '''
-Algoritmo das divisões sucessivas
+Algoritmo das divisões sucessivas recursiva
 '''
 def mdc_r(a: int, b: int) -> int:
   if b == 0:
     return a
   return mdc_r(b, a % b)
 
+'''
+Algoritmo das divisões sucessivas iterativa
+'''
 def mdc_i(a: int, b: int) -> int:
   while b != 0:
     new_b = a % b
