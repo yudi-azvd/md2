85bb5e86cefb610a8ecbffee72d410b2fda172a7
Yudi Yamane <yudiyamane@gmail.com>
2022-07-10 23:35:50 -0300

    chore!: muda nome do verificador para inglês

--- src/check_diophantine.py
+++ src/check_diophantine.py
@@ -1,7 +1,7 @@
 from mdc import mdc_i
 
 
-def check_diofantine(a, b, c, x0, y0, t_interval=20):
+def check_diophantine(a, b, c, x0, y0, t_interval=20):
   max_divisor = mdc_i(a, b)
   if c % max_divisor != 0:
     raise Exception(f'{max_divisor} does not divide {c}')
@@ -21,8 +21,8 @@ def check_diofantine(a, b, c, x0, y0, t_interval=20):
 
 if __name__ == '__main__':
   # L01 Q28
-  # check_diofantine(43, 5, 167, 2*167, -17*167)
+  # check_diophantine(43, 5, 167, 2*167, -17*167)
   # L01 Q29
-  # check_diofantine(119, 272, 1700, 700, -300)
+  # check_diophantine(119, 272, 1700, 700, -300)
   # L01 Q30
-  check_diofantine(6643, 2873, 65000, -80000, 185000)
+  check_diophantine(6643, 2873, 65000, -80000, 185000)
