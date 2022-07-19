004d4a69782505c356bfae83c603e2eb861b9e69
Yudi Yamane <yudiyamane@gmail.com>
2022-07-19 14:21:54 -0300

    feat: adiciona gerador de números primos

--- src/primes.py
+++ src/primes.py
@@ -11,3 +11,26 @@ def is_prime(n: int) -> bool:
     if n % i == 0:
       return False
   return True
+
+
+'''
+
+'''
+class PrimeGenerator():
+  __current_prime = 2
+
+  def __init__(self, starting_number=2) -> None:
+    if not is_prime(starting_number):
+      raise Exception(f'{starting_number} is not prime')
+
+    self.__current_prime = starting_number
+
+  def get_current(self):
+    return self.__current_prime
+  
+  def get_next(self):
+    i = self.__current_prime+1
+    while not is_prime(i):
+      i += 1
+    self.__current_prime = i
+    return self.__current_prime
---
+++ test/test_prime_generator.py
@@ -0,0 +1,26 @@
+import pytest
+from primes import PrimeGenerator
+
+def test_should_return_2_as_current_prime_given_no_args():
+  sut = PrimeGenerator()
+  assert 2 == sut.get_current()
+
+
+def test_should_accept_valid_prime_number_as_starting_number():
+  return None != PrimeGenerator(2)
+
+
+def test_should_throw_exception_if_starting_number_is_not_prime():
+  with pytest.raises(Exception) as e:
+    PrimeGenerator(4)
+  assert 'is not prime' in str(e.value)
+  
+
+def test_should_calculate_next_prime():
+  sut = PrimeGenerator(2)
+  assert 3 == sut.get_next()
+
+
+def test_should_return_13_given_5_as_starting_number():
+  sut = PrimeGenerator(5)
+  assert 7 == sut.get_next()
